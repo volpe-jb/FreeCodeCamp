cb8b62e6bdb17068c0fcc49ffc5754e05616fd3e
JB <volpe-jb@users.noreply.github.com>
2017-05-10 08:31:16 -0500

    Create chessboard.js
    Eloquent Javascript
    8 characters are hardcoded

---
+++ chessboard.js
@@ -0,0 +1,14 @@
+
+for (var i = 0; i <= 7; i++) {
+  counter = 0;
+  
+  counter++;
+  
+  for (var j = 0; i <= 7; i++) {
+   if (i % 2) {
+     console.log("# # # # "); 
+   } else {
+     console.log(" # # # #");
+   }  // end else
+  }  // end if
+}  // end for
