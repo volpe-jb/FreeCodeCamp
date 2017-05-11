8b9bc0c9a070ff24c24023336a14853108b495da
JB <volpe-jb@users.noreply.github.com>
2017-05-11 14:56:44 -0500

    Create factorialze.js
    FCC Challenge

---
+++ factorialze.js
@@ -0,0 +1,22 @@
+/*
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of 
+all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+*/
+
+function factorialize(num) {
+  var sum = 1;
+  
+  for(var i = 1; i <= num; i++) {
+    sum *= i;  
+    console.log(i);
+  }
+  return sum;
+}
+
+factorialize(5);
